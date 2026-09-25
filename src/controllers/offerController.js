1cd2f650bbdcdea4acd42fee73b658c9c09136ee
Mehdi-133 <mehdikarbitou@gmail.com>
2026-09-25 15:11:05 +0000

    YOUAP-111 Load technologies from MySQL into the existing offer form

--- src/controllers/offerController.js
+++ src/controllers/offerController.js
@@ -1,6 +1,7 @@
 import { json } from "body-parser";
 import OfferRepo from "../repositories/offerRepository.js";
 import CompanyRepo from "../repositories/companyRepository.js";
+import TechnologyRepo from "../repositories/technologyRepository.js";
 
 class OfferController {
   async index(req, res) {
@@ -19,12 +20,17 @@ class OfferController {
     }
   }
 
-  new(req, res) {
-    res.render("offer-form/index", {
-      offer: null,
-    });
+  async new(req, res) {
+    try {
+      const technologies = await TechnologyRepo.findAll();
+      res.render("offer-form/index", {
+        offer: null,
+        technologies,
+      });
+    } catch (error) {
+      console.error(" loading error ", error);
+      res.status(500).send(" server error");
+    }
   }
-
 }
-
 export default new OfferController();
--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,17 @@
 import Company from "./Company.js";
-import Offer  from "./Offer.js";
+import Offer from "./Offer.js";
+import Technology from "./Technology.js";
 
-Company.hasMany(Offer , {
-    foreignKey: "company_id",
-    as: "offers",
-})
+Company.hasMany(Offer, {
+  foreignKey: "company_id",
+  as: "offers",
+});
 
 Offer.belongsTo(Company, {
-    foreignKey: "company_id",
-    as: "company"
-})
+  foreignKey: "company_id",
+  as: "company",
+});
 
-export  {Offer , Company}
+
+
+export { Offer, Company , Technology};
